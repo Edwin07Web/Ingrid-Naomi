c293a299c55240467a2ef68ab69a0809f0fb2e78
Edwin - Desarrollador Web <43154540+Edwin07Web@users.noreply.github.com>
2024-07-09 21:42:57 -0600

    Delete Js directory

--- Js/sobre.js
+++
@@ -1,9 +0,0 @@
-var Izquierda = document.getElementById("Izquierda")
-window.addEventListener("scroll",function(){
-    Izquierda.style.left =-window.pageYOffset + "px"
-})
-
-var Derecha = document.getElementById("Derecha")
-window.addEventListener("scroll",function(){
-    Derecha.style.left = window.pageYOffset + "px"
-})
